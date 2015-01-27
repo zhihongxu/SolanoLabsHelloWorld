e872e0864c21bc5dcfe71644c710be0670359166
zhihongxu <zhihong.xu@salesforce.com>
2015-01-27 11:17:08 -0800

    Update project-settings.sh

--- project-settings.sh
+++ project-settings.sh
@@ -2,5 +2,5 @@
 # Default project settings.
 #
 
-scp run.sh distribution@$MAC_MINI_HOST:
-ssh distribution@$MAC_MINI_HOST bash run.sh
+#scp run.sh distribution@$MAC_MINI_HOST:
+#ssh distribution@$MAC_MINI_HOST bash run.sh
